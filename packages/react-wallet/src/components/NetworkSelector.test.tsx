ae0ea8e8a94ab760b6bd5c6c32d248cfc6973c77
aramalipoor <aram.alipoor@gmail.com>
2022-03-21 16:30:18 +0100

    fix: add a basic test

---
+++ packages/react-wallet/src/components/NetworkSelector.test.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { NetworkSelector } from './NetworkSelector';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<NetworkSelector />, div);
+});
