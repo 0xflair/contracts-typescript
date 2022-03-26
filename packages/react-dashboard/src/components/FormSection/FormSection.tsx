a4d0ae38e46cf16af0e71ff6ef39b0f81b22d390
aramalipoor <aram.alipoor@gmail.com>
2022-03-26 15:02:47 +0100

    fix: styling for form header section

--- packages/react-dashboard/src/components/FormSection/FormSection.tsx
+++ packages/react-dashboard/src/components/FormSection/FormSection.tsx
@@ -16,33 +16,35 @@ export const FormSection = (props: Props) => {
   return (
     <div className="mt-10 sm:mt-0">
       <div className="md:grid md:grid-cols-3 md:gap-6">
-        <div className="md:col-span-1 flex items-center gap-x-4">
-          {toggleable ? (
-            <Switch
-              checked={enabled}
-              onChange={setEnabled}
-              className={classNames(
-                enabled ? "bg-indigo-600" : "bg-gray-200",
-                "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-              )}
-            >
-              <span className="sr-only">Use setting</span>
-              <span
-                aria-hidden="true"
+        <div className="md:col-span-1">
+          <div className="flex items-center gap-x-4">
+            {toggleable ? (
+              <Switch
+                checked={enabled}
+                onChange={setEnabled}
                 className={classNames(
-                  enabled ? "translate-x-5" : "translate-x-0",
-                  "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+                  enabled ? "bg-indigo-600" : "bg-gray-200",
+                  "relative inline-flex flex-shrink-0 h-6 w-11 border-2 border-transparent rounded-full cursor-pointer transition-colors ease-in-out duration-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                 )}
-              />
-            </Switch>
-          ) : (
-            ""
-          )}
-          <div className="px-4 sm:px-0">
-            <h3 className="text-lg font-medium leading-6 text-gray-900">
-              {title}
-            </h3>
-            <p className="mt-1 text-sm text-gray-600">{description}</p>
+              >
+                <span className="sr-only">Use setting</span>
+                <span
+                  aria-hidden="true"
+                  className={classNames(
+                    enabled ? "translate-x-5" : "translate-x-0",
+                    "pointer-events-none inline-block h-5 w-5 rounded-full bg-white shadow transform ring-0 transition ease-in-out duration-200"
+                  )}
+                />
+              </Switch>
+            ) : (
+              ""
+            )}
+            <div className="px-4 sm:px-0">
+              <h3 className="text-lg font-medium leading-6 text-gray-900">
+                {title}
+              </h3>
+              <p className="mt-1 text-sm text-gray-600">{description}</p>
+            </div>
           </div>
         </div>
         <div
