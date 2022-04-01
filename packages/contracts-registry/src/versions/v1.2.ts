51f157d30a4ed51412e32a9326734e4ad457ae7e
aramalipoor <aram.alipoor@gmail.com>
2022-04-01 21:38:50 +0200

    fix: use correct contract names

--- packages/contracts-registry/src/versions/v1.2.ts
+++ packages/contracts-registry/src/versions/v1.2.ts
@@ -2,10 +2,10 @@ import ERC721SimpleCollectionArtifact from "contracts-v1.2/collections/ERC721/pr
 import ERC721SimpleCollectionSource from "contracts-v1.2/collections/ERC721/presets/ERC721SimpleCollection.sol";
 import ERC721FullFeaturedCollectionArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollection.json";
 import ERC721FullFeaturedCollectionSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollection.sol";
-import ERC721FullFeaturedCollectionWyvernArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedWyvernCollection.json";
-import ERC721FullFeaturedCollectionWyvernSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedWyvernCollection.sol";
-import ERC721FullFeaturedCollectionZeroExArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedZeroExCollection.json";
-import ERC721FullFeaturedCollectionZeroExSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedZeroExCollection.sol";
+import ERC721FullFeaturedCollectionWyvernArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollectionWyvern.json";
+import ERC721FullFeaturedCollectionWyvernSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollectionWyvern.sol";
+import ERC721FullFeaturedCollectionZeroExArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollectionZeroEx.json";
+import ERC721FullFeaturedCollectionZeroExSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollectionZeroEx.sol";
 
 export const CONTRACTS_V_1_2 = {
   'collections/ERC721/presets/ERC721SimpleCollection': {
