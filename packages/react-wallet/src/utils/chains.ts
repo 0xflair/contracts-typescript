305db7c083eda75f606c8fee995ee66df8f1eea8
aramalipoor <aram.alipoor@gmail.com>
2022-04-01 22:12:23 +0200

    fix: add more chains for flair supported ones

--- packages/react-wallet/src/utils/chains.ts
+++ packages/react-wallet/src/utils/chains.ts
@@ -1,14 +1,23 @@
 import { allChains as WagmiChains, Chain } from "wagmi";
 
-export const FLAIR_CHAIN_IDS = [137 /* polygonMainnet */, 4 /* rinkeby */];
-export const FLAIR_CHAINS: Chain[] = WagmiChains.filter((chain) => FLAIR_CHAIN_IDS.includes(chain.id));
+export const FLAIR_CHAIN_IDS = [
+  1 /* mainnet */, 3 /* ropsten */, 4 /* rinkeby */, 5 /* goerli */,
+  137 /* polygonMainnet */, 80001 /* polygonTestnetMumbai */,
+];
+
+export const FLAIR_CHAINS: Chain[] = WagmiChains.filter((chain) =>
+  FLAIR_CHAIN_IDS.includes(chain.id)
+);
+
 export const FLAIR_DEFAULT_CHAIN = FLAIR_CHAINS.find(
   (c) => c.id === 4 /* rinkeby */
 ) as Chain;
 
 export const FLAIR_CHAIN_MACHINE_NAMES = {
-  1: 'mainnet',
-  4: 'rinkeby',
-  137: 'matic',
-  80001: 'mumbai',
-}
+  1: "mainnet",
+  3: "ropsten",
+  4: "rinkeby",
+  5: "goerli",
+  137: "matic",
+  80001: "mumbai",
+};
