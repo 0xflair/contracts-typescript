5975e2acf44d2b4ad555c1d4616256e8d301ff47
aramalipoor <aram.alipoor@gmail.com>
2022-04-01 20:57:13 +0200

    fix: add suffix for button classes

--- packages/react-dashboard/src/components/ui/Button.tsx
+++ packages/react-dashboard/src/components/ui/Button.tsx
@@ -1,12 +1,12 @@
 import React, { ReactNode } from 'react';
 
-export const PRIMARY =
+export const PRIMARY_BUTTON =
   'inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed';
 
-export const SECONDARY =
+export const SECONDARY_BUTTON =
   'inline-flex items-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed';
 
-export const ACTION =
+export const ACTION_BUTTON =
   'inline-flex items-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50 disabled:cursor-not-allowed';
 
 interface Props {
@@ -23,7 +23,7 @@ export const Button = (props: Props) => {
       disabled={props.disabled}
       onClick={props.onClick}
       type="button"
-      className={props.className || PRIMARY}
+      className={props.className || PRIMARY_BUTTON}
     >
       {props.text}
       {props.icon && <span className="ml-2 h-6 w-6">{props.icon}</span>}
