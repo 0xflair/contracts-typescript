36ee688eca623341cb72e3bf16924a7f20771838
aramalipoor <aram.alipoor@gmail.com>
2022-04-01 21:26:04 +0200

    feat: add simple erc721 collection to the registry

--- packages/contracts-registry/src/versions/v1.2.ts
+++ packages/contracts-registry/src/versions/v1.2.ts
@@ -1,3 +1,5 @@
+import ERC721SimpleCollectionArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721SimpleCollection.json";
+import ERC721SimpleCollectionSource from "contracts-v1.2/collections/ERC721/presets/ERC721SimpleCollection.sol";
 import ERC721FullFeaturedCollectionArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollection.json";
 import ERC721FullFeaturedCollectionSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedCollection.sol";
 import ERC721FullFeaturedCollectionWyvernArtifact from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedWyvernCollection.json";
@@ -6,6 +8,10 @@ import ERC721FullFeaturedCollectionZeroExArtifact from "contracts-v1.2/collectio
 import ERC721FullFeaturedCollectionZeroExSource from "contracts-v1.2/collections/ERC721/presets/ERC721FullFeaturedZeroExCollection.sol";
 
 export const CONTRACTS_V_1_2 = {
+  'collections/ERC721/presets/ERC721SimpleCollection': {
+    artifact: ERC721SimpleCollectionArtifact,
+    source: ERC721SimpleCollectionSource,
+  },
   'collections/ERC721/presets/ERC721FullFeaturedCollection': {
     artifact: ERC721FullFeaturedCollectionArtifact,
     source: ERC721FullFeaturedCollectionSource,
