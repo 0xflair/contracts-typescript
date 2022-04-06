3b44d56e6ff99f4c160798efa420e0992e628e9f
aramalipoor <aram.alipoor@gmail.com>
2022-04-06 19:35:56 +0200

    fix: export require chain component

--- packages/react-wallet/src/components/index.ts
+++ packages/react-wallet/src/components/index.ts
@@ -2,4 +2,5 @@ export * from './NetworkSelector/NetworkSelector';
 export * from './ConnectButton/ConnectButton';
 export * from './RequireConnect/RequireConnect';
 export * from './RequireLogin/RequireLogin';
+export * from './RequireChain/RequireChain';
 export * from './LoginButton/LoginButton';
