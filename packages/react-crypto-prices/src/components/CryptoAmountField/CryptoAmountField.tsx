9feb7fb826a0ad86af6c5ea08357ce0f47a8308f
aramalipoor <aram.alipoor@gmail.com>
2022-04-06 17:56:15 +0200

    feat: allow configuring units for crypto amount field

--- packages/react-crypto-prices/src/components/CryptoAmountField/CryptoAmountField.tsx
+++ packages/react-crypto-prices/src/components/CryptoAmountField/CryptoAmountField.tsx
@@ -1,18 +1,28 @@
+import { utils } from "ethers";
 import * as React from "react";
 import { useNetwork } from "wagmi";
-import { CryptoSymbol } from "../../types";
+import { CryptoSymbol, CryptoUnits } from "../../types";
 import { CryptoPrice } from "../CryptoPrice/CryptoPrice";
 
 export type CryptoAmountFieldProps = {
   label: string;
   description?: React.ReactNode;
-  value: any;
-  onChange: (event: any) => void;
+  value: string;
+  unit?: CryptoUnits;
+  onChange: (value: string) => void;
 };
 
 export const CryptoAmountField = (props: CryptoAmountFieldProps) => {
-  const { label, description, value, onChange } = props;
+  const {
+    label,
+    description,
+    value,
+    unit = CryptoUnits.ETHER,
+    onChange,
+  } = props;
 
+  const convertedValueWei = utils.parseUnits(value, unit);
+  const convertedValueEther = utils.formatEther(convertedValueWei);
   const [{ data: networkData }] = useNetwork();
 
   return (
@@ -23,8 +33,12 @@ export const CryptoAmountField = (props: CryptoAmountFieldProps) => {
           type="text"
           className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-12 sm:text-sm border-gray-300 rounded-md"
           placeholder="0.00"
-          value={value}
-          onChange={onChange}
+          value={convertedValueEther}
+          onChange={(event) =>
+            onChange(
+              utils.formatUnits(utils.parseEther(event.target.value), unit)
+            )
+          }
         />
         <div className="absolute top-2 right-0 pr-3 flex items-center pointer-events-none">
           <span className="text-gray-500 sm:text-sm" id="price-currency">
@@ -32,6 +46,7 @@ export const CryptoAmountField = (props: CryptoAmountFieldProps) => {
             <CryptoPrice
               value={value}
               symbol={networkData.chain?.nativeCurrency?.symbol as CryptoSymbol}
+              unit={unit}
             />{" "}
             USD)
           </span>
