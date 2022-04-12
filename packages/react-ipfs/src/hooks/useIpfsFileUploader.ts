19b718e40702aea5f0de33532cf0dc2f439ff18e
aramalipoor <aram.alipoor@gmail.com>
2022-04-12 17:08:13 +0200

    fix: allow paasing options as args useful when chaining multiple hooks

--- packages/react-ipfs/src/hooks/useIpfsFileUploader.ts
+++ packages/react-ipfs/src/hooks/useIpfsFileUploader.ts
@@ -2,9 +2,7 @@ import axios from "axios";
 import { Environment, useCancel } from "@0xflair/react-common";
 import { useCallback, useEffect, useState } from "react";
 
-import {
-  IpfsUploadResult,
-} from "../types";
+import { IpfsUploadResult } from "../types";
 import { FLAIR_IPFS_BACKEND } from "../constants";
 
 async function ipfsUploadFiles(env: Environment, files: File[]) {
@@ -42,39 +40,48 @@ export function useIpfsFileUploader(options: {
   const [state, setState] = useState<State>(initialState);
 
   const cancelQuery = useCancel();
-  const uploadToIpfs = useCallback(async () => {
-    let didCancel = false;
-    cancelQuery(() => {
-      didCancel = true;
-    });
-
-    if (!fromFile) {
-      if (!didCancel) {
-        setState({
-          error: new Error(`Must provide "File" to useIpfsFileUploader hook`),
-          loading: false,
-        });
-      }
-      return;
-    }
-
-    try {
-      setState({
-        loading: true,
+  const uploadToIpfs = useCallback(
+    async (args?: { fromFile?: File }) => {
+      let didCancel = false;
+      cancelQuery(() => {
+        didCancel = true;
       });
 
-      const result = await ipfsUploadFiles(env, [fromFile]);
-      const ipfsUrl = `ipfs://${result[0].ipfsHash}`;
+      if (!fromFile && !args?.fromFile) {
+        if (!didCancel) {
+          setState({
+            error: new Error(
+              `Must provide "File" to useIpfsFileUploader hook as option or as arg when calling uploadToIpfs`
+            ),
+            loading: false,
+          });
+        }
+        return;
+      }
 
-      if (!didCancel) {
-        setState({ ipfsUrl, loading: false });
+      try {
+        setState({
+          loading: true,
+        });
+
+        const result = await ipfsUploadFiles(env, [
+          (fromFile || args?.fromFile) as File,
+        ]);
+        const ipfsUrl = `ipfs://${result[0].ipfsHash}`;
+
+        if (!didCancel) {
+          setState({ ipfsUrl, loading: false });
+        }
+
+        return ipfsUrl;
+      } catch (error) {
+        if (!didCancel) {
+          setState({ error: error as Error, loading: false });
+        }
       }
-    } catch (error) {
-      if (!didCancel) {
-        setState({ error: error as Error, loading: false });
-      }
-    }
-  }, [cancelQuery, fromFile]);
+    },
+    [cancelQuery, fromFile]
+  );
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
--- packages/react-ipfs/src/hooks/useIpfsJsonUploader.ts
+++ packages/react-ipfs/src/hooks/useIpfsJsonUploader.ts
@@ -4,12 +4,9 @@ import { useCallback, useEffect, useState } from "react";
 
 import { FLAIR_IPFS_BACKEND } from "../constants";
 
-async function ipfsUploadJson(env: Environment, jsonContent: string | Buffer) {
+async function ipfsUploadJson(env: Environment, jsonContent: Record<string, any>) {
   return axios
-    .post<string>(
-      `${FLAIR_IPFS_BACKEND[env]}/v1/ipfs/upload/json`,
-      jsonContent
-    )
+    .post<string>(`${FLAIR_IPFS_BACKEND[env]}/v1/ipfs/upload/json`, jsonContent)
     .then((res) => {
       return res.data;
     });
@@ -28,46 +25,60 @@ const initialState: State = {
 export function useIpfsJsonUploader(options: {
   env?: Environment;
   autoUpload?: boolean;
-  jsonContent?: string | Buffer;
+  jsonContent?: Record<string, any>;
 }) {
-  const { env = Environment.PROD, autoUpload, jsonContent } = options;
+  const {
+    env = Environment.PROD,
+    autoUpload,
+    jsonContent,
+  } = options;
 
   const [state, setState] = useState<State>(initialState);
 
   const cancelQuery = useCancel();
-  const uploadToIpfs = useCallback(async () => {
-    let didCancel = false;
-    cancelQuery(() => {
-      didCancel = true;
-    });
-
-    if (!jsonContent) {
-      if (!didCancel) {
-        setState({
-          error: new Error(`Must provide "jsonContent" to useIpfsContentUploader hook`),
-          loading: false,
-        });
-      }
-      return;
-    }
-
-    try {
-      setState({
-        loading: true,
+  const uploadToIpfs = useCallback(
+    async (args?: { jsonContent?: Record<string, any> }) => {
+      let didCancel = false;
+      cancelQuery(() => {
+        didCancel = true;
       });
 
-      const ipfsHash = await ipfsUploadJson(env, jsonContent);
-      const ipfsUrl = `ipfs://${ipfsHash}`;
+      if (!jsonContent && !args?.jsonContent) {
+        if (!didCancel) {
+          setState({
+            error: new Error(
+              `Must provide "jsonContent" to useIpfsContentUploader hook as option or as arg when calling uploadToIpfs`
+            ),
+            loading: false,
+          });
+        }
+        return;
+      }
 
-      if (!didCancel) {
-        setState({ ipfsUrl, loading: false });
+      try {
+        setState({
+          loading: true,
+        });
+
+        const ipfsHash = await ipfsUploadJson(
+          env,
+          (args?.jsonContent || jsonContent) as Record<string, any>
+        );
+        const ipfsUrl = `ipfs://${ipfsHash}`;
+
+        if (!didCancel) {
+          setState({ ipfsUrl, loading: false });
+        }
+
+        return ipfsUrl;
+      } catch (error) {
+        if (!didCancel) {
+          setState({ error: error as Error, loading: false });
+        }
       }
-    } catch (error) {
-      if (!didCancel) {
-        setState({ error: error as Error, loading: false });
-      }
-    }
-  }, [cancelQuery, jsonContent]);
+    },
+    [cancelQuery, jsonContent]
+  );
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => {
