cec0e7e6124d1540762b340891161c8e0dc12a88
aramalipoor <aram.alipoor@gmail.com>
2022-04-16 17:10:32 +0200

    feat: allow a title for error component

--- packages/react-ui/src/components/elements/Errors.tsx
+++ packages/react-ui/src/components/elements/Errors.tsx
@@ -5,81 +5,81 @@ import {
 } from "@0xflair/react-common";
 
 interface Props {
+  title?: string;
+  className?: string;
   error?: string | string[] | null | Error | any;
 }
 
-export class Errors extends React.Component<Props> {
-  render() {
-    const { error } = this.props;
+export const Errors = ({
+  title,
+  error,
+  className = "errors flex flex-col gap-2 text-sm rounded-md bg-red-100 p-2 text-red-800",
+}: Props) => {
+  let errorsList: React.ReactNode = null;
 
-    const mainClass = "errors text-sm rounded-md bg-red-100 p-2 text-red-800";
-
-    if (typeof error === "string") {
-      return (
-        <ul className={mainClass}>
-          <li>{error}</li>
+  if (typeof error === "string") {
+    errorsList = (
+      <ul>
+        <li>{error}</li>
+      </ul>
+    );
+  } else if (error instanceof Array) {
+    errorsList = (
+      <ul>
+        {error.map((e: React.ReactNode) => (
+          <li>
+            <Errors error={e} />
+          </li>
+        ))}
+      </ul>
+    );
+  } else if (typeof error === "object") {
+    if (error === null) {
+      errorsList = null;
+    } else if (error.response) {
+      switch (error.response?.data?.code) {
+        case "GITHUB_BAD_CREDENTIALS":
+          errorsList = (
+            <ul>
+              <li>
+                Make sure configured GitHub personal access token is correct
+              </li>
+              <li>
+                If part of a GitHub organization, make sure personal access
+                token has "Enabled SSO"
+              </li>
+            </ul>
+          );
+          break;
+        default:
+          errorsList = (
+            <ul>
+              <li>
+                {error.response?.data?.message ||
+                  `Unexpected error with status ${error.status}`}
+              </li>
+            </ul>
+          );
+      }
+    } else if (error instanceof Error) {
+      errorsList = (
+        <ul>
+          <li>{translateContractError(extractHumanReadableError(error))}</li>
         </ul>
       );
-    }
-
-    if (error instanceof Array) {
-      return (
-        <ul className={mainClass}>
-          {error.map((e: React.ReactNode) => (
-            <li>
-              <Errors error={e} />
-            </li>
-          ))}
-        </ul>
-      );
-    }
-
-    if (typeof error === "object") {
-      if (error === null) {
-        return "";
-      }
-
-      if (error.response) {
-        switch (error.response?.data?.code) {
-          case "GITHUB_BAD_CREDENTIALS":
-            return (
-              <ul className={mainClass}>
-                <li>
-                  Make sure configured GitHub personal access token is correct
-                </li>
-                <li>
-                  If part of a GitHub organization, make sure personal access
-                  token has "Enabled SSO"
-                </li>
-              </ul>
-            );
-          default:
-            return (
-              <ul className={mainClass}>
-                <li>
-                  {error.response?.data?.message ||
-                    `Unexpected error with status ${error.status}`}
-                </li>
-              </ul>
-            );
-        }
-      }
-
-      if (error instanceof Error) {
-        return (
-          <ul className={mainClass}>
-            <li>{translateContractError(extractHumanReadableError(error))}</li>
-          </ul>
-        );
-      }
-
-      return (
-        <ul className={mainClass}>
+    } else {
+      errorsList = (
+        <ul>
           <li>{translateContractError(extractHumanReadableError(error))}</li>
         </ul>
       );
     }
-
-    return "";
   }
-}
+
+  return (
+    <div className={className}>
+      {title && <h3 className="font-bold">{title}</h3>}
+      {errorsList}
+    </div>
+  );
+};
