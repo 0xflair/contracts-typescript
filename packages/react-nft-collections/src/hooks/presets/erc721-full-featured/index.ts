3efa8354a25f3630007221becf6d6403da1c506e
aramalipoor <aram.alipoor@gmail.com>
2022-04-25 13:28:37 +0200

    fix: add full featured collection type

--- packages/react-nft-collections/src/hooks/presets/erc721-full-featured/index.ts
+++ packages/react-nft-collections/src/hooks/presets/erc721-full-featured/index.ts
@@ -1,2 +1,4 @@
+export * from './types';
+
 export * from './useERC721FullFeaturedDeployer';
 export * from './useERC721FullFeaturedContractInfo';
---
+++ packages/react-nft-collections/src/hooks/presets/erc721-full-featured/types.ts
@@ -0,0 +1,18 @@
+export type ERC721FullFeaturedCollectionConfig = {
+  collectionName?: string;
+  collectionSymbol?: string;
+  collectionDescription?: string;
+  collectionImageUri?: string;
+  collectionMetadataUri?: string;
+  maxSupply?: string;
+  placeholderName?: string;
+  placeholderDescription?: string;
+  placeholderImageUri?: string;
+  placeholderMetadataUri?: string;
+  preSalePrice?: string;
+  preSalePerWallet?: string;
+  publicSalePrice?: string;
+  publicSalePerTx?: string;
+  royaltyAddress?: string;
+  royaltyPercent?: string;
+};
