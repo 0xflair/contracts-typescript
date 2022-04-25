26e98b0bd6f5394f0427f607da23473e349cf09c
aramalipoor <aram.alipoor@gmail.com>
2022-04-25 12:40:31 +0200

    fix: add coingecko provider to flair provider

--- packages/flair-sdk/src/react/provider.tsx
+++ packages/flair-sdk/src/react/provider.tsx
@@ -1,4 +1,6 @@
+import { CoinGeckoProvider } from '@0xflair/react-coingecko';
 import { WalletProvider } from '@0xflair/react-wallet';
+import * as React from 'react';
 
 type Config = {
   appName?: string;
@@ -14,12 +16,14 @@ export const FlairProvider = (props: Props) => {
   const { children, appName, infuraId, wagmiOverrides } = props;
 
   return (
-    <WalletProvider
-      appName={appName}
-      infuraId={infuraId}
-      wagmiOverrides={wagmiOverrides}
-    >
-      {children}
-    </WalletProvider>
+    <CoinGeckoProvider>
+      <WalletProvider
+        appName={appName}
+        infuraId={infuraId}
+        wagmiOverrides={wagmiOverrides}
+      >
+        {children}
+      </WalletProvider>
+    </CoinGeckoProvider>
   );
 };
