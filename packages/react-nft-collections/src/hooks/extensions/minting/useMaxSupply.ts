dd8bd7327cdb60121a7695d000f4422ad6332bbd
aramalipoor <aram.alipoor@gmail.com>
2022-05-08 13:06:06 +0200

    feat: add v1.5 contracts

--- packages/react-nft-collections/src/hooks/extensions/minting/useMaxSupply.ts
+++ packages/react-nft-collections/src/hooks/extensions/minting/useMaxSupply.ts
@@ -1,7 +1,7 @@
-import { Version } from '@0xflair/contracts-registry';
-import { ReadContractConfig, useContractRead } from '@0xflair/react-common';
-import { Provider } from '@ethersproject/providers';
-import { BigNumberish, Signer } from 'ethers';
+import { Version } from "@0xflair/contracts-registry";
+import { ReadContractConfig, useContractRead } from "@0xflair/react-common";
+import { Provider } from "@ethersproject/providers";
+import { BigNumberish, Signer } from "ethers";
 
 type Config = Partial<ReadContractConfig> & {
   version?: Version;
@@ -17,8 +17,8 @@ export const useMaxSupply = ({
 }: Config) => {
   return useContractRead<BigNumberish>({
     version,
-    contractKey: 'collections/ERC721/extensions/ERC721AutoIdMinterExtension',
-    functionName: 'maxSupply',
+    contractKey: "collections/ERC721/extensions/ERC721AutoIdMinterExtension",
+    functionName: "maxSupply",
     contractAddress,
     signerOrProvider,
     ...restOfConfig,
--- packages/react-nft-collections/src/hooks/extensions/minting/useTotalSupply.ts
+++ packages/react-nft-collections/src/hooks/extensions/minting/useTotalSupply.ts
@@ -1,7 +1,7 @@
-import { Version } from '@0xflair/contracts-registry';
-import { ReadContractConfig, useContractRead } from '@0xflair/react-common';
-import { Provider } from '@ethersproject/providers';
-import { BigNumberish, Signer } from 'ethers';
+import { Version } from "@0xflair/contracts-registry";
+import { ReadContractConfig, useContractRead } from "@0xflair/react-common";
+import { Provider } from "@ethersproject/providers";
+import { BigNumberish, Signer } from "ethers";
 
 type Config = Partial<ReadContractConfig> & {
   version?: Version;
@@ -17,8 +17,8 @@ export const useTotalSupply = ({
 }: Config) => {
   return useContractRead<BigNumberish>({
     version,
-    contractKey: 'collections/ERC721/extensions/ERC721AutoIdMinterExtension',
-    functionName: 'totalSupply',
+    contractKey: "collections/ERC721/extensions/ERC721AutoIdMinterExtension",
+    functionName: "totalSupply",
     contractAddress,
     signerOrProvider,
     ...restOfConfig,
