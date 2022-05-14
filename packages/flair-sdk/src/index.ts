7b3e5ebf0426bacca8563ea6fca5f8ac13baeff5
aramalipoor <aram.alipoor@gmail.com>
2022-05-14 20:20:44 +0200

    fix: export react meta-transactions hooks

--- packages/flair-sdk/src/index.ts
+++ packages/flair-sdk/src/index.ts
@@ -7,6 +7,7 @@ export * from '@0xflair/react-coingecko';
 export * from '@0xflair/react-common';
 export * from '@0xflair/react-icons';
 export * from '@0xflair/react-ipfs';
+export * from '@0xflair/react-meta-transactions';
 export * from '@0xflair/react-nft-collections';
 export * from '@0xflair/react-openzeppelin';
 export * from '@0xflair/react-ui';
