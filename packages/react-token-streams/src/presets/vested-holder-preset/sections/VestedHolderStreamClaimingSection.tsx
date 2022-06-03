76fef4a5be021db1e40609e512da06d7109b7d13
aramalipoor <aram.alipoor@gmail.com>
2022-06-04 00:09:15 +0200

    fix: remove require chain component from claiming section

--- packages/react-token-streams/src/presets/vested-holder-preset/sections/VestedHolderStreamClaimingSection.tsx
+++ packages/react-token-streams/src/presets/vested-holder-preset/sections/VestedHolderStreamClaimingSection.tsx
@@ -11,8 +11,8 @@ import { VestedHolderStreamClaimingProvider } from '../providers';
 
 type Props = {
   env?: Environment;
-  chainId?: number;
-  contractAddress?: string;
+  chainId: number;
+  contractAddress: string;
 };
 
 export const VestedHolderStreamClaimingSection = ({
@@ -20,77 +20,67 @@ export const VestedHolderStreamClaimingSection = ({
   chainId,
   contractAddress,
 }: Props) => {
-  if (!chainId) {
-    return <div>Missing parameter "chainId" in the URL</div>;
-  }
-
-  if (!contractAddress) {
-    return <div>Missing parameter "contractAddress" in the URL</div>;
-  }
-
   return (
-    <RequireChain requiredChainId={Number(chainId)}>
-      <StreamClaimingProvider
-        env={env}
-        chainId={Number(chainId)}
-        contractAddress={contractAddress}
-      >
-        <VestedHolderStreamClaimingProvider>
-          {({ data: { stream } }) => (
-            <>
-              <style type="text/css">{`html {background: none transparent !important;}`}</style>
-              <main className="h-fit mx-auto max-w-lg flex items-center p-4">
-                <div className="min-w-full">
-                  <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
-                    <div className="lg:col-span-12">
-                      <div className="flex gap-4 sm:items-center sm:justify-between sm:flex-row flex-col">
-                        <div className="flex flex-col gap-2">
-                          <h1 className="text-2xl font-medium text-gray-900">
-                            {stream?.publicTitle ||
-                              stream?.contractAddress?.substring(0, 8)}
-                          </h1>
-                        </div>
-                        <div>
-                          <div className="text-md font-medium text-gray-900">
-                            <VestedHolderStreamRate />
-                          </div>
-                        </div>
+    <StreamClaimingProvider
+      env={env}
+      chainId={Number(chainId)}
+      contractAddress={contractAddress}
+    >
+      <VestedHolderStreamClaimingProvider>
+        {({ data: { stream } }) => (
+          <>
+            <style type="text/css">{`html {background: none transparent !important;}`}</style>
+            <main className="h-fit mx-auto max-w-lg flex items-center p-4">
+              <div className="min-w-full">
+                <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
+                  <div className="lg:col-span-12">
+                    <div className="flex gap-4 sm:items-center sm:justify-between sm:flex-row flex-col">
+                      <div className="flex flex-col gap-2">
+                        <h1 className="text-2xl font-medium text-gray-900">
+                          {stream?.publicTitle ||
+                            stream?.contractAddress?.substring(0, 8)}
+                        </h1>
                       </div>
-
-                      <div className="mt-6">
-                        <section className="mt-6 bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8 lg:mt-0 lg:col-span-5">
-                          <VestedHolderStreamInformation />
-
-                          <div className="mt-6 flex flex-col justify-center items-center">
-                            <StreamClaimButton />
-                            <DisconnectButton className="text-indigo-700 text-sm mt-4" />
-                          </div>
-                        </section>
+                      <div>
+                        <div className="text-md font-medium text-gray-900">
+                          <VestedHolderStreamRate />
+                        </div>
                       </div>
                     </div>
 
-                    <div className="mt-4 lg:col-span-12">
-                      <StreamStatusBar />
+                    <div className="mt-6">
+                      <section className="mt-6 bg-gray-50 rounded-lg px-4 py-6 sm:p-6 lg:p-8 lg:mt-0 lg:col-span-5">
+                        <VestedHolderStreamInformation />
 
-                      {stream?.publicDescription ? (
-                        <div className="mt-4">
-                          <h2 className="mt-4 text-sm font-medium text-gray-900">
-                            Description
-                          </h2>
-
-                          <div className="mt-3 prose prose-sm text-gray-500">
-                            {stream?.publicDescription}
-                          </div>
+                        <div className="mt-6 flex flex-col justify-center items-center">
+                          <StreamClaimButton />
+                          <DisconnectButton className="text-indigo-700 text-sm mt-4" />
                         </div>
-                      ) : null}
+                      </section>
                     </div>
                   </div>
+
+                  <div className="mt-4 lg:col-span-12">
+                    <StreamStatusBar />
+
+                    {stream?.publicDescription ? (
+                      <div className="mt-4">
+                        <h2 className="mt-4 text-sm font-medium text-gray-900">
+                          Description
+                        </h2>
+
+                        <div className="mt-3 prose prose-sm text-gray-500">
+                          {stream?.publicDescription}
+                        </div>
+                      </div>
+                    ) : null}
+                  </div>
                 </div>
-              </main>
-            </>
-          )}
-        </VestedHolderStreamClaimingProvider>
-      </StreamClaimingProvider>
-    </RequireChain>
+              </div>
+            </main>
+          </>
+        )}
+      </VestedHolderStreamClaimingProvider>
+    </StreamClaimingProvider>
   );
 };
