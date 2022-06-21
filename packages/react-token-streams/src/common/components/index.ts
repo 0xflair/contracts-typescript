2889f94caf4101b6b25cbbf1a7404500ea7fc739
aramalipoor <aram.alipoor@gmail.com>
2022-06-22 01:07:54 +0200

    fix: export new components for token streams

--- packages/react-token-streams/src/common/components/index.ts
+++ packages/react-token-streams/src/common/components/index.ts
@@ -1,3 +1,6 @@
+export * from './StreamClaimableAmount';
 export * from './StreamClaimButton';
+export * from './StreamEligibleNfts';
 export * from './StreamInformation';
 export * from './StreamStatusBar';
+export * from './StreamTotalClaimed';
--- packages/react-token-streams/src/presets/vested-holder-preset/components/index.tsx
+++ packages/react-token-streams/src/presets/vested-holder-preset/components/index.tsx
@@ -1,3 +1,5 @@
 export * from './VestedHolderStreamInformation';
 export * from './VestedHolderStreamRate';
+export * from './VestedHolderStreamReleasedAmount';
 export * from './VestedHolderStreamStatusBar';
+export * from './VestedHolderStreamTimeUnit';
