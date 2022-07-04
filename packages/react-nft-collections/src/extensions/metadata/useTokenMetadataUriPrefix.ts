82592ad093391bb1e7f309e1cd4aa4b970b6e0d9
aramalipoor <aram.alipoor@gmail.com>
2022-07-04 22:33:04 +0200

    fix: use bytes type

--- packages/react-nft-collections/src/extensions/metadata/useTokenMetadataUriPrefix.ts
+++ packages/react-nft-collections/src/extensions/metadata/useTokenMetadataUriPrefix.ts
@@ -2,9 +2,10 @@ import {
   FeatureReadByTagConfig,
   useFeatureReadByTag,
 } from '@0xflair/react-common';
+import { BytesLike } from 'ethers';
 
 export const useTokenMetadataUriPrefix = (config: FeatureReadByTagConfig) => {
-  return useFeatureReadByTag<boolean>({
+  return useFeatureReadByTag<BytesLike>({
     tag: 'token_metadata_uri_prefix',
     ...config,
   });
--- packages/react-nft-collections/src/extensions/metadata/useTokenMetadataUriSuffix.ts
+++ packages/react-nft-collections/src/extensions/metadata/useTokenMetadataUriSuffix.ts
@@ -2,9 +2,10 @@ import {
   FeatureReadByTagConfig,
   useFeatureReadByTag,
 } from '@0xflair/react-common';
+import { BytesLike } from 'ethers';
 
 export const useTokenMetadataUriSuffix = (config: FeatureReadByTagConfig) => {
-  return useFeatureReadByTag<boolean>({
+  return useFeatureReadByTag<BytesLike>({
     tag: 'token_metadata_uri_suffix',
     ...config,
   });
