27b33c1affa9d6e736a2c0e4d108d188f22e0140
aramalipoor <aram.alipoor@gmail.com>
2022-07-13 20:32:30 +0200

    fix: bump up the contracts version

--- packages/react-nft-collections/src/extensions/sales/components/CollectionSalesActiveStatus.tsx
+++ packages/react-nft-collections/src/extensions/sales/components/CollectionSalesActiveStatus.tsx
@@ -17,17 +17,17 @@ export const CollectionSalesActiveStatus = ({ className }: Props) => {
           Sold out
         </span>
       ) : null}
-      {preSaleStatus && !publicSaleStatus ? (
+      {!soldOut && preSaleStatus && !publicSaleStatus ? (
         <span className="sale-status pre-sale pre-sale-active inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
           Pre-sale is active
         </span>
       ) : null}
-      {publicSaleStatus ? (
+      {!soldOut && publicSaleStatus ? (
         <span className="sale-status public-sale public-sale-active inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-green-100 text-green-800">
           Public-sale is active
         </span>
       ) : null}
-      {!preSaleStatus && !publicSaleStatus && !mintLoading ? (
+      {!soldOut && !preSaleStatus && !publicSaleStatus && !mintLoading ? (
         <span className="sale-status sale-not-active inline-flex items-center px-3 py-0.5 rounded-full text-sm font-medium bg-yellow-100 text-yellow-800">
           Sale not active yet
         </span>
