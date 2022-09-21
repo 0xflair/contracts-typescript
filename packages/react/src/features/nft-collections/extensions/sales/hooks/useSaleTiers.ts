e453dfd1f5ddf6e4a7610a8178c375474c20210e
aramalipoor <aram.alipoor@gmail.com>
2022-09-21 11:59:23 +0200

    feat: remove simple sales and support new architecture

--- packages/react/src/features/nft-collections/extensions/sales/hooks/useSaleTiers.ts
+++ packages/react/src/features/nft-collections/extensions/sales/hooks/useSaleTiers.ts
@@ -4,12 +4,8 @@ import { BigNumberish, BytesLike } from 'ethers';
 import { useCallback, useMemo, useState } from 'react';
 import { useProvider } from 'wagmi';
 
-import {
-  PredefinedReadContractConfig,
-  useHasAnyOfFeatures,
-} from '../../../../../common';
+import { PredefinedReadContractConfig } from '../../../../../common';
 import { Tier } from '../types';
-import { useSimpleSaleMinter } from './useSimpleSaleMinter';
 import { useTierSaleAllowlistChecker } from './useTierSaleAllowlistChecker';
 import { useTierSaleEligibleAmount } from './useTierSaleEligibleAmount';
 
@@ -24,32 +20,6 @@ export const useSaleTiers = (config: Config) => {
   const [error, setError] = useState<Error | string>();
   const [isLoading, setIsLoading] = useState(false);
   const [tiers, setTiers] = useState<Record<number, Tier>>([]);
-
-  const {
-    data: supportsSimpleSales,
-    error: supportsSimpleSalesError,
-    isLoading: supportsSimpleSalesLoading,
-  } = useHasAnyOfFeatures({
-    env: config.env,
-    chainId: config.chainId,
-    contractAddress: config.contractAddress,
-    tags: [
-      'erc721_public_sale_extension',
-      'erc721_presale_extension',
-      'mint_amount_by_merkle_proof',
-    ],
-  });
-
-  const {
-    data: supportsTieredSales,
-    error: supportsTieredSalesError,
-    isLoading: supportsTieredSalesLoading,
-  } = useHasAnyOfFeatures({
-    env: config.env,
-    chainId: config.chainId,
-    contractAddress: config.contractAddress,
-    tags: ['erc721_tiering_extension', 'mint_by_tier_with_allowance_and_proof'],
-  });
 
   const {
     error: allowlistCheckerError,
@@ -72,42 +42,6 @@ export const useSaleTiers = (config: Config) => {
     contractAddress: config.contractAddress,
     minterAddress: config.minterAddress,
     enabled: false,
-  });
-
-  const {
-    data: {
-      preSaleStatus,
-      preSalePrice,
-      preSaleMaxMintPerWallet,
-      preSaleIsAllowlisted,
-      publicSaleStatus,
-      publicSalePrice,
-      publicSaleMaxMintPerTx,
-    },
-    error: {
-      preSaleStatusError,
-      preSaleAllowlistCheckerError,
-      preSaleMaxMintPerWalletError,
-      preSaleMintError,
-      publicSaleMaxMintPerTxError,
-      publicSaleMintError,
-      publicSaleStatusError,
-    },
-    isLoading: {
-      preSaleAllowlistCheckerLoading,
-      preSaleMaxMintPerWalletLoading,
-      preSaleMintLoading,
-      preSaleStatusLoading,
-      publicSaleMaxMintPerTxLoading,
-      publicSaleMintLoading,
-      publicSaleStatusLoading,
-    },
-  } = useSimpleSaleMinter({
-    chainId: config.chainId,
-    contractAddress: config.contractAddress,
-    contractVersion: config.contractVersion,
-    minterAddress: config.minterAddress,
-    enabled: supportsSimpleSales,
   });
 
   const provider = useProvider({
@@ -190,136 +124,45 @@ export const useSaleTiers = (config: Config) => {
     setError(undefined);
 
     try {
-      if (supportsTieredSales) {
-        const fetchedTiers: Record<number, Tier> = {};
+      const fetchedTiers: Record<number, Tier> = {};
 
-        for (let i = 0, l = 10; i < l; i++) {
-          const tier = await fetchTierById(i);
+      for (let i = 0, l = 10; i < l; i++) {
+        const tier = await fetchTierById(i);
 
-          if (
-            tier &&
-            tier.maxPerWallet &&
-            Number(tier.maxPerWallet.toString()) > 0
-          ) {
-            fetchedTiers[i] = tier;
-          } else {
-            break;
-          }
+        if (
+          tier &&
+          tier.maxPerWallet &&
+          Number(tier.maxPerWallet.toString()) > 0
+        ) {
+          fetchedTiers[i] = tier;
+        } else {
+          break;
         }
-
-        setTiers(fetchedTiers);
-      } else if (supportsSimpleSales) {
-        const past = +new Date() - 100 * 60 * 60 * 1000;
-        const future = +new Date() + 100 * 60 * 60 * 1000;
-        const preSaleTier: Tier = {
-          start: preSaleStatus ? past : future,
-          end: future,
-          currency: ZERO_BYTES32,
-          maxAllocation: Infinity,
-          maxPerWallet: preSaleMaxMintPerWallet || Infinity,
-          merkleRoot: ZERO_BYTES32,
-          hasAllowlist: true,
-          price: preSalePrice || 0,
-          isSavedOnChain: true,
-          reserved: Infinity,
-          isActive: preSaleStatus,
-          isAllowlisted: preSaleIsAllowlisted,
-          isEligible: preSaleStatus && preSaleIsAllowlisted,
-          eligibleAmount: preSaleMaxMintPerWallet,
-          minterAddress: config.minterAddress,
-        };
-        const publicSaleTier: Tier = {
-          start: publicSaleStatus ? past : future,
-          end: future,
-          currency: ZERO_BYTES32,
-          maxAllocation: Infinity,
-          maxPerWallet: publicSaleMaxMintPerTx || Infinity,
-          merkleRoot: ZERO_BYTES32,
-          hasAllowlist: false,
-          price: publicSalePrice || 0,
-          isSavedOnChain: true,
-          reserved: Infinity,
-          isActive: publicSaleStatus,
-          isAllowlisted: undefined,
-          isEligible: publicSaleStatus,
-          eligibleAmount: publicSaleMaxMintPerTx,
-          minterAddress: config.minterAddress,
-        };
-
-        setTiers({
-          0: preSaleTier,
-          1: publicSaleTier,
-        });
       }
+
+      setTiers(fetchedTiers);
     } catch (error: any) {
       setError(error);
     }
 
     setIsLoading(false);
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    config.minterAddress,
-    supportsSimpleSales,
-    supportsTieredSales,
-    preSaleAllowlistCheckerLoading,
-    preSaleMintLoading,
-    preSaleStatusLoading,
-    publicSaleStatusLoading,
-    preSaleStatus,
-    preSaleIsAllowlisted,
-    publicSaleStatus,
-  ]);
-
-  useMemo(() => {
-    if (supportsSimpleSalesLoading || supportsTieredSalesLoading) {
-      return;
-    }
-
-    refetchTiers();
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [
-    config.minterAddress,
-    supportsSimpleSalesLoading,
-    supportsTieredSalesLoading,
-    preSaleStatus,
-    preSaleIsAllowlisted,
-    publicSaleStatus,
-  ]);
+  }, [config.minterAddress]);
 
   return {
     data: tiers,
     error:
       // top-level
       error ||
-      supportsSimpleSalesError ||
-      supportsTieredSalesError ||
       // tiered sales
       allowlistCheckerError ||
-      eligibleAmountError ||
-      // simple sales
-      preSaleStatusError ||
-      preSaleAllowlistCheckerError ||
-      preSaleMaxMintPerWalletError ||
-      preSaleMintError ||
-      publicSaleMaxMintPerTxError ||
-      publicSaleMintError ||
-      publicSaleStatusError,
+      eligibleAmountError,
     isLoading:
       // top-level
       isLoading ||
-      supportsSimpleSalesLoading ||
-      supportsTieredSalesLoading ||
       // tiered sales
       allowlistCheckerLoading ||
-      eligibleAmountLoading ||
-      // simple sales
-      preSaleStatusLoading ||
-      preSaleAllowlistCheckerLoading ||
-      preSaleMaxMintPerWalletLoading ||
-      preSaleMintLoading ||
-      publicSaleMaxMintPerTxLoading ||
-      publicSaleMintLoading ||
-      publicSaleStatusLoading,
+      eligibleAmountLoading,
     refetchTiers,
   };
 };
