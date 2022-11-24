7f08e1d85c4d6560f1b2291f6897248c72d5ffa4
aramalipoor <aram.alipoor@gmail.com>
2022-11-24 14:54:00 +0100

    chore: add and export meta transaction object type

--- packages/common/src/types/index.ts
+++ packages/common/src/types/index.ts
@@ -1,3 +1,4 @@
 export * from './env';
+export * from './error';
 export * from './transactions';
 export * from './wallets';
--- packages/metatx/src/types/meta-transaction.ts
+++ packages/metatx/src/types/meta-transaction.ts
@@ -1,15 +1,58 @@
+import { Log } from '@ethersproject/providers';
+import { StandardError, TransactionEvent } from '@flair-sdk/common';
+import { BigNumberish } from 'ethers';
+
+export enum MetaTransactionState {
+  CREATED = 'created',
+  QUEUED = 'queued',
+  ATTEMPTED = 'attempted',
+  SUBMITTED = 'submitted',
+  PROCESSED = 'processed',
+  WAITING = 'waiting',
+  MINED = 'mined',
+  REVERTED = 'reverted',
+  FAILED = 'failed',
+}
+
 export type MetaTransaction = {
-  forwarder: string;
-  from: string;
-  to: string;
-  value: string;
-  minGasPrice: string;
-  maxGasPrice: string;
-  expiresAt: number;
-  nonce: string;
-  data: string;
-  signature: string;
-  submittedAt: number;
-  processedAt?: number;
-  transaction?: string;
+  id?: string;
+
+  clientId?: string;
+  chainId?: number;
+  forwarder?: string;
+  from?: string;
+  to?: string;
+  value?: string;
+  minGasPrice?: string;
+  maxGasPrice?: string;
+  expiresAt?: number;
+  nonce?: string;
+  data?: string;
+
+  signature?: string;
+
+  createdAt?: number; // 0) Create a new meta-transaction in the database
+  queuedAt?: number; // 1) Added to the queue
+  attemptedAt?: number; // 2) Attempt to send to relayer
+  submittedAt: number; // 3) Submitted fully to the relayer
+  processedAt?: number; // 4) Relayer accepted the tx and gave us a txHash
+  waitAt?: number; // 5) Waiting for transaction to be mined
+  minedAt?: number; // 5) Transaction is fully mined (either failed or succeeded)
+
+  state?: MetaTransactionState;
+  lastError?: StandardError;
+
+  txHash?: string;
+  txGasPrice?: string;
+  txNonce?: string;
+  txReceipt: {
+    status: number;
+    gasUsed: BigNumberish;
+    effectiveGasPrice: BigNumberish;
+    blockNumber: BigNumberish;
+    blockHash: string;
+    confirmations: number;
+    logs: Log[];
+  };
+  events: TransactionEvent[];
 };
