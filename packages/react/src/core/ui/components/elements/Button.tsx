3d0e1ff807ae75b62e26d990c69f8da5e1a7406b
aramalipoor <aram.alipoor@gmail.com>
2022-12-28 19:13:38 +0100

    fix: add danger button class

--- packages/react/src/core/ui/components/elements/Button.tsx
+++ packages/react/src/core/ui/components/elements/Button.tsx
@@ -6,6 +6,9 @@ export const PRIMARY_BUTTON =
 export const SECONDARY_BUTTON =
   'inline-flex items-center whitespace-nowrap px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-indigo-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 disabled:opacity-50 disabled:cursor-not-allowed';
 
+export const DANGER_BUTTON =
+  'inline-flex items-center whitespace-nowrap px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-red-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 disabled:opacity-50 disabled:cursor-not-allowed';
+
 export const ACTION_BUTTON =
   'inline-flex items-center whitespace-nowrap px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500 disabled:opacity-50 disabled:cursor-not-allowed';
 
